c7c6066f1c86519bb2aa7ab439e12ac31f665087
michiko swiggs <michiko23@gmail.com>
2012-09-30 22:46:53 -0700

    set up account pages

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@
   
   
   $("#featured").orbit();
-  
+    
 
   // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
   // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
