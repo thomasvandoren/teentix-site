fd56c94dd0c272b5898137c75c3acc2354936ea7
michiko swiggs <michiko@swiggs.com>
2013-02-27 20:37:04 -0800

    blog stuff

--- css/my_styles.js
+++ css/my_styles.js
@@ -4,6 +4,7 @@ CKEDITOR.stylesSet.add( 'my_styles',
     { name: 'Body Copy', element: 'p' },
     { name: 'Big Headline', element: 'h3', attributes: { 'class': 'big_headline' } },
     { name: 'Medium Title' , element: 'h4', attributes: { 'class': 'medium_headline' } },
+    { name: 'Photo Caption' , element: 'p', attributes: { 'class': 'entry-image-caption' } },
 
     // Inline Styles
     { name: 'Image Left', element: 'img', attributes: { 'class': 'float_left' } },
