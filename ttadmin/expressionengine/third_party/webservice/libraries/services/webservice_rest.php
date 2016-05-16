1292c835e088cc56bc4bcdec8bae78a6b859ba67
Thomas Van Doren <thomas.vandoren@gmail.com>
2016-05-15 17:47:11 -0700

    Use underscores in headers... just to make things simpler.

--- ttadmin/expressionengine/third_party/webservice/libraries/services/webservice_rest.php
+++ ttadmin/expressionengine/third_party/webservice/libraries/services/webservice_rest.php
@@ -910,7 +910,7 @@ class Webservice_rest
         {
             if (substr($name, 0, 5) == 'HTTP_')
             {
-                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                $headers[str_replace(' ', '_', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
             }
         }
         return $headers;
