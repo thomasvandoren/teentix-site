bdb609e6b3dacf09ca277e1abe4886357e9332be
Thomas Van Doren <thomas.vandoren@gmail.com>
2018-07-24 17:32:24 -0700

    Add CORS headers to all requests.

--- index.php
+++ index.php
@@ -99,6 +99,16 @@
  * --------------------------------------------------------------------
  */
 
+/*
+ * CORS
+ */
+// TODO: filter origins? (thomasvandoren, 2018-07-24)
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+    header('Access-Control-Allow-Headers: *');
+    header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH, HEAD');
+}
+
 
 /*
  * ---------------------------------------------------------------
